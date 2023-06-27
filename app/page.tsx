541e67ceccb94f6d4b48a3ce996c9e3b1f727b08
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-06-27 15:27:33 +0300

    Finished with footer and added title for catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Hero/>
+      <Hero />
+      <div className='mt-12 padding-x padding-y max-width' id="discover"><div className='home__text-container'>
+        <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+      <p>Explore the cars you might like</p></div></div>
     </main>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,9 +22,8 @@ const hundleScroll = ()=>{}
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay"></div>
-        </div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" /></div>
+          <div className="hero__image-overlay"></div>     
       </div>
     </div>
   );
