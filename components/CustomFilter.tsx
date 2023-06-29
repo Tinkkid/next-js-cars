7c49be7c24a2e98c2539e04b50af447ae4108cb8
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-06-29 16:30:55 +0300

    Added filter selected

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client';
 
-const CustomFilter = () => {
+import { useState, Fragment } from 'react'
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { Listbox, Transition } from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
+
+const CustomFilter = ({title, year}:CustomFilterProps) => {
+  const [selected, setSelected] = useState();
   return (
-    <div>
-      test1
+    <div className='w-fit'>
+      <Listbox
+        value={selected}
+      onChange={(e)=>setSelected(e)}>
+        <div className="w-fir z-10 relative">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+            alt="chevron up down"/>
+          </Listbox.Button>
+        </div>
+      </Listbox>
     </div>
   )
 }
